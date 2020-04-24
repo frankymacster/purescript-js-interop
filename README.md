931b138a272b717b5e8bc54ca5dcb2f91c9d4573
frankymacster <fs.sunatori@me.com>
2020-04-24 13:37:11 -0400

    since pulp build didn't work with purescript@0.13.6, redo pulp init with purescript@0.11.7

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://notes.maciejsmolinski.com/2018/06/10/integrate-purescript-with-your-java
 1. open 2 terminal tabs
 2. in one, run `yarn run dev`
 3. in the other, run `pulp --watch build`
+
+psc-package:
+https://readthedocs.org/projects/psc-package/downloads/pdf/latest/
--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,9 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: Effect Unit
+main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
